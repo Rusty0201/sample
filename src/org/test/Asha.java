5f698b8e45869e4e0bf22e8f2e4bd05ad54850fb
Rusty0201 <ashamano263@gmail.com>
2022-11-21 11:22:04 +0530

    send a push

---
+++ src/org/test/Asha.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Asha {
+	public void name() {
+		System.out.println("Priya");
+		
+	}
+	public static void main(String[] args) {
+		Asha a=new Asha();
+		a.name();
+		
+	}
+
+}
