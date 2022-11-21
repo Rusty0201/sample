8c7766339800a8ed0eadb6f35e49a9e5ddf825fd
Rusty0201 <ashamano263@gmail.com>
2022-11-21 12:10:13 +0530

    new change

--- src/org/test/Asha.java
+++ src/org/test/Asha.java
@@ -3,7 +3,7 @@ package org.test;
 public class Asha {
 	public void name() {
 		System.out.println("Priya");
-		
+		System.out.println("xcvvb");
 	}
 	public static void main(String[] args) {
 		Asha a=new Asha();
